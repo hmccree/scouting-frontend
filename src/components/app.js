e3980c99cb3ef766651234e395725ac8954e90e3
Alex Coffin <alexcoffin1999@gmail.com>
2017-11-07 17:20:15 -0800

    Added 404 Error Page (#1)
    * Added 404 Error Page
    
    Added GIF/video to the 404 Page
    
    Techincally it is a video, but for all intents and purposes it is a
    beautiful GIF.
    
    * Simplified CSS and HTML Tree
    
    * Caleb is picky

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import { h } from 'preact'
 import { Router } from 'preact-router'
 
 import Home from '../routes/home'
+import Error404 from '../routes/404'
 
 export default () => (
   <div id="app">
     <Router>
       <Home path="/" />
+      <Error404 default />
     </Router>
   </div>
 )
