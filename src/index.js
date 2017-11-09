356e73539bbd7ac2e7f34d83688576457d166323
Caleb Eby <caleb.eby01@gmail.com>
2017-11-08 20:24:27 -0800

    Unregister service workers

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import App from './components/app'
 import { render, h } from 'preact'
 
 render(<App />, document.body)
+
+const unregiesterSW = async () => {
+  if ('serviceWorker' in navigator) {
+    ;(await navigator.serviceWorker.getRegistrations()).forEach(sw =>
+      sw.unregister().then(console.log)
+    )
+  }
+}
+
+unregiesterSW()
