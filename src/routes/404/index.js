20b8b0e15ae665f172d48fe466c1795819974cfc
Alex Coffin <alexcoffin1999@gmail.com>
2017-11-27 17:08:41 -0800

    Centered the 404 page (#9)

--- src/routes/404/index.js
+++ src/routes/404/index.js
@@ -1,7 +1,8 @@
 import { h } from 'preact'
+import style from './style'
 
 export default ({ eventId }) => (
-  <div>
+  <div class={style.main404}>
     <h1>ERROR 404</h1>
     <p>The requested file could not be found.</p>
     <video autoplay loop type="video/mp4" src="/assets/videos/burning_robot_404.mp4" />
