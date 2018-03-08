9fa95131ac87fb60a2c648700c067f179aab0a9d
Franklin Harding <32021905+fharding1@users.noreply.github.com>
2018-03-07 16:34:36 -0800

    Add links to TBA in analysis, make team picker bigger, add reports number (#78)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,31 @@
 import { h } from 'preact'
 import { header, back as backClass } from './style.sss'
 import Icon from '../icon'
+import { route } from 'preact-router'
 
 interface HeaderProps {
   title?: string
   back?: string
   contents?: any
+  verify?: boolean
 }
 
-const Header = ({ title, back, contents }: HeaderProps) => (
+const Header = ({ title, back, contents, verify }: HeaderProps) => (
   <header class={header}>
     {back && (
-      <a class={backClass} href={back}>
+      <a
+        class={backClass}
+        onClick={e => {
+          if (verify === true) {
+            e.stopImmediatePropagation()
+            e.preventDefault()
+            if (confirm('Are you sure you want to leave?')) {
+              route(back)
+            }
+          }
+        }}
+        href={back}
+      >
         <Icon icon="left" />
       </a>
     )}
