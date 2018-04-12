6794104272a0f3978ed7cabc95d2c896c352368d
Caleb Eby <caleb.eby01@gmail.com>
2018-04-12 16:39:38 -0700

    Revert "Gray out right and left navigation buttons on match page. (#112)"
    This reverts commit 542eb75788a812632e9b81420706102960338daf.

--- src/routes/match/index.tsx
+++ src/routes/match/index.tsx
@@ -110,13 +110,15 @@ const Match = ({ eventId, matchId }: { eventId: string; matchId: string }) => (
                 <Icon icon="left" />
               </a>
             ) : (
-              <a class={navigationClass}>
-                <Icon icon="left" fill="gray" />
-              </a>
+              <div />
             )}
             {match !== undefined ? (
               <div class={icons}>
-                <a href={`/events/${eventId}/${matchId}/print`} target="_blank">
+                <a
+                  onClick={() =>
+                    window.open(`/events/${eventId}/${matchId}/print`)
+                  }
+                >
                   <Icon icon="print" />
                 </a>
 
@@ -144,9 +146,7 @@ const Match = ({ eventId, matchId }: { eventId: string; matchId: string }) => (
                 <Icon icon="right" />
               </a>
             ) : (
-              <a class={navigationClass}>
-                <Icon icon="right" fill="gray" />
-              </a>
+              <div />
             )}
           </div>
         </div>
